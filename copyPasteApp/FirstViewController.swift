67936511541322f37a361cf347d3d2a13584f624
yuki_mitsudome <everything.yu23@gmail.com>
2017-09-12 15:03:11 +0900

    Init commit copy pasteしまくるためだけのアプリ

---
+++ copyPasteApp/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  copyPasteApp
+//
+//  Created by 有希 on 2017/09/12.
+//  Copyright © 2017年 Yuki Mitsudome. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var forDesign: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+
+
+}
+
---
+++ copyPasteApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  copyPasteApp
+//
+//  Created by 有希 on 2017/09/12.
+//  Copyright © 2017年 Yuki Mitsudome. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
